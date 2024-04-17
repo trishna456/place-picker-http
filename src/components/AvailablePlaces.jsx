5203ca3a6d4c1b1a97b51d21a493da93c3da311a
Trishna <trishna.patil13@gmail.com>
2024-04-16 20:19:30 -0400

    using async await to fet data, adding fallback code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,8 +3,20 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+    /*
     fetch("http://localhost:3000/places")
       .then((response) => {
         return response.json(); //this returns another promise
@@ -12,12 +24,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
       .then((resData) => {
         setAvailablePlaces(resData.places);
       });
+    */
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
