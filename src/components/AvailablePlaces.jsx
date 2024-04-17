b5458a8315d39dc57724cf6cb91531ceb4e1c414
Trishna <trishna.patil13@gmail.com>
2024-04-16 20:29:37 -0400

    added error boundries and fallback code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,16 +1,30 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/place");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places.");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places. Please try again later.",
+        });
+      }
+
       setIsFetching(false);
     }
 
@@ -26,6 +40,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
       });
     */
   }, []);
+
+  if (error) {
+    return <Error title={"An error occurred!"} message={error.message} />;
+  }
 
   return (
     <Places
